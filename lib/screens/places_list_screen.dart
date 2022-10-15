28fae19a94b88758c7f73fec6f52ac2f16d51c0d
DhanushDivakar <93920313+DhanushDivakar@users.noreply.github.com>
2022-10-15 20:19:58 +0530

    initial

--- lib/screens/places_list_screen.dart
+++ lib/screens/places_list_screen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:great_places/providers/great_places.dart';
 import 'package:great_places/screens/add_place_screen.dart';
+import 'package:provider/provider.dart';
+
 //this is the first scren
 class PlacesListScreen extends StatelessWidget {
   const PlacesListScreen({Key key}) : super(key: key);
@@ -19,7 +22,21 @@ class PlacesListScreen extends StatelessWidget {
         ],
       ),
       body: Center(
-        child: CircularProgressIndicator(),
+        child: Consumer<GreatPlaces>(
+          child: Center(
+            child: const Text("Got no places yet start adding some"),
+          ),
+          builder: (ctx, greatPlaces, ch) => greatPlaces.items.length <= 0
+              ? ch
+              : ListView.builder(
+                  itemCount: greatPlaces.items.length,
+                  itemBuilder: (ctx, i) => ListTile(
+                    leading: CircleAvatar(backgroundImage: FileImage(greatPlaces.items[i].image),),
+                    title: Text(greatPlaces.items[i].title),
+                    onTap: () {},
+                  ),
+                ),
+        ),
       ),
     );
   }
--- lib/widgets/image_input.dart
+++ lib/widgets/image_input.dart
@@ -2,8 +2,12 @@ import 'package:flutter/material.dart';
 import 'dart:io';
 
 import 'package:image_picker/image_picker.dart';
+import 'package:path/path.dart' as path;
+import 'package:path_provider/path_provider.dart' as syspaths;
 
 class ImageInput extends StatefulWidget {
+  final Function onSelectImage;
+  ImageInput(this.onSelectImage);
   @override
   State<ImageInput> createState() => _ImageInputState();
 }
@@ -12,11 +16,18 @@ class _ImageInputState extends State<ImageInput> {
   File _storedImage;
 
   Future<void> _takePicture() async {
-    final picker = ImagePicker();
-    final imageFile = await picker.getImage(
+
+    final imageFile = await ImagePicker.pickImage(
       source: ImageSource.camera,
       maxWidth: 600,
     );
+    setState(() {
+      _storedImage = File(imageFile.path);
+    });
+    final appDir = await syspaths.getApplicationDocumentsDirectory();
+    final fileName = path.basename(imageFile.path);
+    final savedImage = await  imageFile.copy('${appDir.path}/$fileName');//copy the file into the appdir.path and keep the fileName
+    widget.onSelectImage(savedImage);
   }
 
   @override
